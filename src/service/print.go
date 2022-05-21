3acef8e60b2ae15839da6981ef4b76af2d88c139
Ashwani Sharma <i14a23h19a@gmail.com>
2022-05-21 19:24:05 +0530

    feat: make API public

--- src/service/print.go
+++ src/service/print.go
@@ -23,7 +23,7 @@ type printRequest struct {
 
 // setupResponse setups all the common HTTP response headers
 func setupResponse(w *http.ResponseWriter) {
-	(*w).Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
 	(*w).Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
 	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 	(*w).Header().Set("Content-Type", "image/png")
