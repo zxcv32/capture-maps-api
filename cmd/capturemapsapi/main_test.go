113d8d666985cf3b00dea6fc3fff2e6787017ba6
Ashwani Sharma <ashwani@zxcv32.com>
2022-08-20 16:22:25 +0530

    test: fix main test

--- cmd/capturemapsapi/main_test.go
+++ cmd/capturemapsapi/main_test.go
@@ -17,15 +17,13 @@ func TestHome(t *testing.T) {
 	handler := http.HandlerFunc(Home)
 	handler.ServeHTTP(rr, req)
 
-	if status := rr.Code; status != http.StatusMovedPermanently {
+	if status := rr.Code; status != http.StatusSeeOther {
 		t.Errorf("handler returned wrong status code: got %v want %v",
 			status, http.StatusMovedPermanently)
 	}
 
-	expected := `<a href="http://zxcv32.com">Moved Permanently</a>.`
 	body := strings.TrimSpace(rr.Body.String())
-	if body != expected {
-		t.Errorf("handler returned unexpected body: got %v want %v",
-			rr.Body.String(), expected)
+	if len(body) < 1 {
+		t.Error("handler returned unexpected body")
 	}
 }
